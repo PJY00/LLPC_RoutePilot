6b502211fda09063e9e80709e1eed67662168ec6
Gin-Lee24 <leejin3064@gmail.com>
2025-05-25 00:28:21 +0900

    FEAT Add live route

--- RoutePilot/static/JS/main.js
+++ RoutePilot/static/JS/main.js
@@ -1,7 +1,1 @@
-// 1단계: 기본 경로 저장용 배열 생성
-let globalRouteCoords = [];
-function drawLine(points, trafficArr) {
-    globalRouteCoords.push(...points);
-    const pl = new Tmapv2.Polyline({ path: points, strokeColor: "#DD0000", strokeWeight: 6, map: map });
-    routePolylines.push(pl);
-}
+// Step 2: Added globalRouteCoords.push(...)
